5a37ed623a5dfc41730b793f3a4e2bd6ce7f7576
Eugene Grybinnyk <eugeneg@backbase.com>
2025-01-07 00:15:37 +0100

    Add logging to tests

--- year2015/day01/day01_test.go
+++ year2015/day01/day01_test.go
@@ -3,16 +3,18 @@ package year2015day01
 import "testing"
 
 func TestPart1(t *testing.T) {
-	want := "280"
 	got := Part1(&input)
+	t.Log("Part 1:", got)
+	want := "280"
 	if got != want {
 		t.Errorf("Part1: \ngot %s\nwant %s", got, want)
 	}
 }
 
 func TestPart2(t *testing.T) {
-	want := "1797"
 	got := Part2(&input)
+	t.Log("Part 2:", got)
+	want := "1797"
 	if got != want {
 		t.Errorf("Part2: \ngot %s\nwant %s", got, want)
 	}
