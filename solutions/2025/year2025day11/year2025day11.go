4a70e88efbbcc91eac20fd38df8798c08f6882ca
Eugene Grybinnyk <eugeneg@backbase.com>
2025-12-11 15:59:32 +0100

    2025 Day 11 (optimization, removed cycle detection)

--- solutions/2025/year2025day11/year2025day11.go
+++ solutions/2025/year2025day11/year2025day11.go
@@ -20,8 +20,7 @@ func init() {
 func part1(input string) (string, error) {
 	devices := parseInput(input)
 	memo := map[string]map[string]int{}
-	seen := map[string]struct{}{}
-	result := searchAllPaths(devices, "you", "out", seen, memo)
+	result := searchAllPaths(devices, "you", "out", memo)
 	return strconv.Itoa(result), nil
 }
 
@@ -30,22 +29,22 @@ func part2(input string) (string, error) {
 	memo := map[string]map[string]int{}
 
 	result := 0
-	fftDac := searchAllPaths(devices, "fft", "dac", map[string]struct{}{}, memo)
+	fftDac := searchAllPaths(devices, "fft", "dac", memo)
 	if fftDac == 0 { // svr -> dac -> fft -> out
-		svrDac := searchAllPaths(devices, "svr", "dac", map[string]struct{}{"fft": {}}, memo)
-		dacFft := searchAllPaths(devices, "dac", "fft", map[string]struct{}{}, memo)
-		fftOut := searchAllPaths(devices, "fft", "out", map[string]struct{}{"dac": {}}, memo)
+		svrDac := searchAllPaths(devices, "svr", "dac", memo)
+		dacFft := searchAllPaths(devices, "dac", "fft", memo)
+		fftOut := searchAllPaths(devices, "fft", "out", memo)
 		result = svrDac * dacFft * fftOut
 	} else { // svr -> fft -> dac -> out
-		svrFft := searchAllPaths(devices, "svr", "fft", map[string]struct{}{"dac": {}}, memo)
-		dacOut := searchAllPaths(devices, "dac", "out", map[string]struct{}{"fft": {}}, memo)
+		svrFft := searchAllPaths(devices, "svr", "fft", memo)
+		dacOut := searchAllPaths(devices, "dac", "out", memo)
 		result = svrFft * fftDac * dacOut
 	}
 
 	return strconv.Itoa(result), nil
 }
 
-func searchAllPaths(devices map[string][]string, from, to string, seen map[string]struct{}, memo map[string]map[string]int) int {
+func searchAllPaths(devices map[string][]string, from, to string, memo map[string]map[string]int) int {
 	if from == to {
 		return 1
 	}
@@ -60,18 +59,12 @@ func searchAllPaths(devices map[string][]string, from, to string, seen map[strin
 		return val
 	}
 
-	seen[from] = struct{}{}
-
-	totalPaths := 0
+	allPaths := 0
 	for _, connection := range devices[from] {
-		if _, ok := seen[connection]; !ok {
-			paths2 := searchAllPaths(devices, connection, to, seen, memo)
-			totalPaths += paths2
-		}
+		allPaths += searchAllPaths(devices, connection, to, memo)
 	}
-	delete(seen, from)
-	cache[to] = totalPaths
-	return totalPaths
+	cache[to] = allPaths
+	return allPaths
 }
 
 func parseInput(input string) map[string][]string {
