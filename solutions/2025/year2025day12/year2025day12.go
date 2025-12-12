de1f9a4719fad60305e9bf8a3a03fe1544d69b14
Eugene Grybinnyk <eugeneg@backbase.com>
2025-12-12 11:09:16 +0100

    2025 Day 12

--- solutions/2025/year2025day12/year2025day12.go
+++ solutions/2025/year2025day12/year2025day12.go
@@ -2,9 +2,12 @@ package year2025day12
 
 import (
 	_ "embed"
+	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/polarfish/advent-of-code-go/tools/registry"
+	"github.com/polarfish/advent-of-code-go/tools/utils"
 )
 
 //go:embed year2025day12.txt
@@ -16,9 +19,67 @@ func init() {
 }
 
 func part1(input string) (string, error) {
-	return strconv.Itoa(0), nil
+	parts := strings.Split(input, "\n\n")
+	regionsParts := utils.Lines(parts[len(parts)-1])
+
+	regions := make([]region, len(regionsParts))
+	for i, regionPart := range regionsParts {
+		regionSplit := strings.Split(regionPart, ": ")
+		regionSizeSplit := strings.Split(regionSplit[0], "x")
+		width, err := strconv.Atoi(regionSizeSplit[0])
+		if err != nil {
+			return "", utils.ErrBadInput
+		}
+		height, err := strconv.Atoi(regionSizeSplit[1])
+		if err != nil {
+			return "", utils.ErrBadInput
+		}
+		regionShapesSplit := strings.Split(regionSplit[1], " ")
+		regionShapes := make([]int, len(regionShapesSplit))
+
+		for j, shapeStr := range regionShapesSplit {
+			shape, err := strconv.Atoi(shapeStr)
+			if err != nil {
+				return "", utils.ErrBadInput
+			}
+			regionShapes[j] = shape
+		}
+
+		regions[i] = region{
+			width:  width,
+			height: height,
+			shapes: regionShapes,
+		}
+	}
+
+	no := 0
+	yes := 0
+	maybe := 0
+	for _, r := range regions {
+		totalShapes := 0
+		for _, s := range r.shapes {
+			totalShapes += s
+		}
+
+		if r.width*r.height >= totalShapes*9 {
+			yes++
+		} else if r.width*r.height < totalShapes*7 {
+			no++
+		} else {
+			maybe++
+		}
+	}
+
+	fmt.Println(yes, no, maybe)
+
+	return strconv.Itoa(yes), nil
 }
 
 func part2(input string) (string, error) {
 	return strconv.Itoa(0), nil
+}
+
+type region struct {
+	width, height int
+	shapes        []int
 }
--- solutions/2025/year2025day12/year2025day12_test.go
+++ solutions/2025/year2025day12/year2025day12_test.go
@@ -6,8 +6,48 @@ import (
 	"github.com/polarfish/advent-of-code-go/tools/test"
 )
 
+var sample = `
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
+`
+
+func TestYear2025Day12Part1Sample(t *testing.T) {
+	test.Assert(t, "1", part1, sample) // it should be 2
+}
+
 func TestYear2025Day12Part1(t *testing.T) {
-	test.Assert(t, "0", part1, input)
+	test.Assert(t, "567", part1, input)
 }
 
 func TestYear2025Day12Part2(t *testing.T) {
