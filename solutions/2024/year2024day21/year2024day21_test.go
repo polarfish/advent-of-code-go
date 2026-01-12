12fce413c3a575a9ef9b7dede6c1a08f1b668c0e
Eugene Grybinnyk <eugene.grybinnyk@gmail.com>
2026-01-12 17:05:41 +0100

    2024 Day 21 (added tests for the sample)

--- solutions/2024/year2024day21/year2024day21_test.go
+++ solutions/2024/year2024day21/year2024day21_test.go
@@ -6,10 +6,20 @@ import (
 	"github.com/polarfish/advent-of-code-go/tools/test"
 )
 
+var sample = `
+029A
+980A
+179A
+456A
+379A
+`
+
 func TestYear2024Day21Part1(t *testing.T) {
+	test.Assert(t, "126384", part1, sample)
 	test.Assert(t, "156714", part1, input)
 }
 
 func TestYear2024Day21Part2(t *testing.T) {
+	test.Assert(t, "154115708116294", part2, sample)
 	test.Assert(t, "191139369248202", part2, input)
 }
